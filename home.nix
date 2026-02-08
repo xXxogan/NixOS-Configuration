5507233ad4bfbd42acac158253e401404a2a7f5b
Vyacheslav <yarok2005@mail.ru>
2026-02-08 22:53:57 +0700

    Revise home.nix for home-manager and git setup
    Updated home-manager configuration and added git settings.

--- home.nix
+++ home.nix
@@ -1,24 +1,32 @@
 { config, pkgs, username, homeDirectory, ... }: {
-	home = {
-		inherit username homeDirectory;
-		stateVersion = "25.05";
-		
-		packages = with pkgs; [
-			neofetch
-			htop
-			git
-		];
-	};
-	programs.bash = {
-		enable = true;
-		shellAliases = 
-		let
-			flakePath = "${homeDirectory}/nixos-config"; 
-		in {
-			rebuild = "sudo nixos-rebuild switch --flake ${flakePath}#nixos";
-			hms = "home-manager switch --flake ${flakePath}#xogan";
-			v = "vim";
-		};
-	};
+  
+  home = {
+    inherit username homeDirectory;
+    stateVersion = "24.11"; # Должна совпадать с версией home-manager в flake inputs
+    
+    packages = with pkgs; [
+      neofetch
+      htop
+      # git # git уже есть в systemPackages, но можно оставить и тут для локальной настройки
+    ];
+  };
 
+  programs.bash = {
+    enable = true;
+    shellAliases = 
+      let
+        flakePath = "${homeDirectory}/nixos-config"; 
+      in {
+        # Обновляем всё сразу одной командой
+        rebuild = "sudo nixos-rebuild switch --flake ${flakePath}#nixos";
+        v = "vim";
+      };
+  };
+
+  # Чтобы git работал корректно, лучше настроить его через HM
+  programs.git = {
+    enable = true;
+    userName = "Xogan";
+    userEmail = "youremail@example.com";
+  };
 }
