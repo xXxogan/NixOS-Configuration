8aec55075f180370e4d7b51d281d586186125c38
Vyacheslav <yarok2005@mail.ru>
2026-02-08 22:53:17 +0700

    Refactor NixOS configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,44 +1,34 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# your system.
 
-{ config, pkgs, lib, home-manager, username, homeDirectory ... }:
+{ config, pkgs, lib, home-manager, username, homeDirectory, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
-      home-manager.nixModules.home-manager
       ./hardware-configuration.nix
+      home-manager.nixModules.home-manager
     ];
 
+  # --- Настройки Home Manager ---
   home-manager.enable = true;
   home-manager.useUserPackages = true;
-  home-manager.users.${username} = import ./home.nix { inherit config pkgs lib username homeDirectory; };
+  # Передаем аргументы (pkgs, lib и т.д.) автоматически через extraSpecialArgs, если нужно
+  home-manager.users.${username} = import ./home.nix;
   
-  users.users.${username} = {
-    isNormalUser = true;
-    home = homeDirectory;
-    extraGroups = [ "wheel" "networkmanager" ];
-  };
+  # Если в home.nix нужны username/homeDirectory, их можно передать так:
+  home-manager.extraSpecialArgs = { inherit username homeDirectory; };
 
-  # Bootloader.
+  # --- Загрузчик ---
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # --- Сеть ---
+  networking.hostName = "nixos"; 
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # --- Локаль и Время ---
   time.timeZone = "Asia/Novosibirsk";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "ru_RU.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -53,28 +43,28 @@
     LC_TIME = "ru_RU.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # ! ВАЖНО: Шрифты консоли для русского языка
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "ru"; 
+  };
+
+  # --- Графическая среда (GNOME) ---
   services.xserver = {
     enable = true;
     videoDrivers = [ "modesetting" ];
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    
+    # ! ВАЖНО: Настройка клавиатуры (US + RU) и переключение по Alt+Shift
+    xkb = {
+      layout = "us,ru";
+      variant = "";
+      options = "grp:lctrl_lshift_toggle";
+    };
   };
 
-  systemd.services.vmtoolsd.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # --- Звук ---
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -82,77 +72,45 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.xogan = {
+  # --- Пользователь (Объединенный блок) ---
+  users.users.${username} = {
     isNormalUser = true;
     description = "Xogan";
+    home = homeDirectory;
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird 
+      # thunderbird 
     ];
   };
 
-  # Enable automatic login for the user.
+  # --- Автологин ---
   services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "xogan";
+  services.displayManager.autoLogin.user = username;
 
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  # Workaround for GNOME autologin issue
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
-  # Install firefox.
+  # --- Пакеты и Программы ---
   programs.firefox.enable = true;
-
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-   wget
-   open-vm-tools
-   home-manager
-   git
+    vim 
+    wget
+    git
+    open-vm-tools
+    # home-manager # Не обязательно ставить как пакет, если он подключен модулем, но можно оставить
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # VM Tools (Для VMware/Parallels)
+  systemd.services.vmtoolsd.enable = true;
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  # --- SSH и Nix Settings ---
   services.openssh.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
- 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05"; # Используй версию, с которой ставишь систему
 }
