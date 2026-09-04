82fc407d42a3386e5b0ebc529ea36f29cc8d7e6d
Vyacheslav <yarok2005@mail.ru>
2026-09-04 15:14:39 +0700

    Edit flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,22 @@
 {
-
-	description = "My system configuration";
-
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.05";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
-
-	outputs = { nixpkgs, home-manager, ... }: 
-		let
-			system = "aarch64-linux";
-			username = "xogan";
-			homeDirectory = "/home/${username}";
-		in {
-		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-			inherit system;
-			specialArgs = { inherit home-manager username homeDirectory; };
-			modules = [ ./configuration.nix ];
-		};
-	};
+  description = "My system configuration";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-linux";
+      username = "xogan";
+      homeDirectory = "/home/${username}";
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit home-manager username homeDirectory; };
+        modules = [ ./configuration.nix ];
+      };
+    };
 }
